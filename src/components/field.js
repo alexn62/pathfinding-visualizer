7f638991ee4a66818c2dc0cc595c0ee7fcd5ec73
Alex Nussbaumer <alex.nssbmr@gmail.com>
2022-03-17 20:36:18 +0100

    more styiling touches

--- src/components/field.js
+++ src/components/field.js
@@ -11,26 +11,30 @@ const Field = ({ matrix }) => {
       row.push(
         <div
           key={[i, j]}
-          className={`h-2 w-2 md:w-3 md:h-3 lg:w-5 lg:h-5 transition-all duration-200 ${
-            matrix[i][j] === WALL
-              ? 'bg-black'
-              : matrix[i][j] === SHORTESTPATH
-              ? 'bg-green-500'
-              : matrix[i][j] === STARTMARKER
-              ? 'bg-green-500'
-              : matrix[i][j] === TARGETMARKER
-              ? 'bg-red-500'
-              : matrix[i][j] === VISITEDFIELD
-              ? 'bg-purple-400'
-              : matrix[i][j] === TOUCHEDFIELD
-              ? 'bg-pink-500'
-              : 'bg-gray-400'
-          }`}
+          // className={`transition-all h-4 w-4
+          className={`h-2 w-2 md:w-3 md:h-3 lg:w-4 lg:h-4 transition-all duration-200
+           ${
+             matrix[i][j] === WALL
+               ? 'bg-black'
+               : matrix[i][j] === SHORTESTPATH
+               ? 'bg-green-500'
+               : matrix[i][j] === STARTMARKER
+               ? 'bg-green-500'
+               : matrix[i][j] === TARGETMARKER
+               ? 'bg-red-500'
+               : matrix[i][j] === VISITEDFIELD
+               ? 'bg-purple-400'
+               : matrix[i][j] === TOUCHEDFIELD
+               ? 'bg-pink-500'
+               : 'bg-gray-400'
+           }
+          `}
         ></div>
       );
     }
     tempRows.push(
-      <div key={i} className="flex flex-row">
+      <div key={i} className="flex flex-row w-full">
+        {/* <div key={i} className="flex flex-row bg-red"> */}
         {row}
       </div>
     );
@@ -38,6 +42,7 @@ const Field = ({ matrix }) => {
   // setRows(tempRows)
   return (
     <div className="flex justify-center">
+      {/* <div className="border-8 border-black">{tempRows}</div> */}
       <div className="flex flex-col items-center border-8 border-black w-min">{tempRows}</div>
     </div>
   );
